c3fd590147b8a80798976525dfcce1154a600e97
sghosh1810 <sghosh1810@gmail.com>
2021-10-07 12:30:11 +0530

    Cors test

--- server.js
+++ server.js
@@ -44,6 +44,9 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
+  setHeaders: {
+    'Access-Control-Allow-Origin':'watchanime.dev'
+  }
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
