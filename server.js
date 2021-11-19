192cf54254da17f38517654d92336e6c169d85ba
sghosh1810 <sghosh1810@gmail.com>
2021-11-19 23:47:45 +0530

    Test Header

--- server.js
+++ server.js
@@ -44,6 +44,9 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
+  setHeaders: {
+    'referer' : 'https://gogoplay1.com/',
+  },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
